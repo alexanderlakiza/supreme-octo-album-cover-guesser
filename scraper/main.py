efd68373075af463965f86678533217c775d049f
Alexander Lakiza <alelakiza@yandex.ru>
2022-11-13 18:22:11 +0300

    Updated main scraper code

--- scraper/main.py
+++ scraper/main.py
@@ -1,24 +1,28 @@
 import json
+import numpy as np
 import os
+import PIL
 import random
+import requests
 import time
 import urllib.request
 from datetime import datetime
+from PIL import Image
 
 import deezer
 
 client = deezer.Client(headers={'Accept-Language': 'en'})
 
-all_genres = [
-    'Jazz',
-    'R&B',
-    'Pop',
-    'Rap/Hip Hop',
-    'Alternative',
-    'Rock',
-    'Metal',
-    'Electronic'
-]
+all_genres = {
+    'Jazz': 0,
+    'R&B': 1,
+    'Pop': 2,
+    'Rap/Hip Hop': 3,
+    'Alternative': 4,
+    'Rock': 5,
+    'Metal': 6,
+    'Electronic': 7
+}
 
 BASE_DIR = '../data/'
 
@@ -28,9 +32,6 @@ target_dict = {}
 
 with open(f'{BASE_DIR}targets.json', 'w') as json_file:
     json.dump(target_dict, json_file)
-
-with open(f'{BASE_DIR}targets.json', 'r') as json_file:
-    target_dict = json.load(json_file)
 
 
 def show_cur_time(endpoint):
@@ -43,8 +44,11 @@ def show_cur_time(endpoint):
 print(show_cur_time('Start'))
 
 deezer_genres = client.list_genres()
-for n_genre, genre in enumerate(all_genres):
+for genre in (all_genres.keys()):
     print(f"{genre=}")
+
+    n_genre = all_genres[genre]
+    genre_dict = {}
 
     for d_genre in deezer_genres:
         if d_genre.name == genre:
@@ -56,22 +60,44 @@ for n_genre, genre in enumerate(all_genres):
     for n_artist, artist in enumerate(current_genre_artists):
         print(f"{n_artist=}")
         artist_albums = artist.get_albums()
-        if len(artist_albums) > 13:  # if len(albums) <= 15 then fetch all albums, otherwise fetch only 15
-            fetched_artist_albums = random.sample(list(artist_albums), 13)
+        if len(artist_albums) > 18:  # if len(albums) <= 18 then fetch all albums, otherwise fetch only 18
+            fetched_artist_albums = random.sample(list(artist_albums), 18)
         else:
             fetched_artist_albums = artist_albums
 
+        albums_arrays = []
+
         for album in fetched_artist_albums:
-
             if album.genres and album.genres[0].name == genre:
-                current_filename = f"{BASE_DIR}images/{n_image}.jpg"
-                urllib.request.urlretrieve(album.cover_big, current_filename)
-                n_image += 1
-                target_dict[current_filename] = n_genre
 
-                time.sleep(0.4)
+                try:
+                    album_im = Image.open(requests.get(album.cover_big, stream=True).raw)
 
-print(show_cur_time('Start'))
+                    for album_array in albums_arrays:
+                        if (np.array(album_im) == album_array).all():
+                            break
+                    else:
+                        current_filename = f"{BASE_DIR}images/{n_image}.jpg"
 
-with open(f'{BASE_DIR}targets.json', 'w') as json_file:
-    json.dump(target_dict, json_file)
+                        with open(f'{BASE_DIR}targets.json', 'r') as json_file:
+                            target_dict = json.load(json_file)
+
+                        try:
+                            urllib.request.urlretrieve(album.cover_big, current_filename)
+                            target_dict[current_filename] = n_genre
+                            n_image += 1
+
+                            cur_album_image = Image.open(current_filename)
+                            albums_arrays.append(np.array(cur_album_image))
+
+                            with open(f'{BASE_DIR}targets.json', 'w') as json_file:
+                                json.dump(target_dict, json_file)
+
+                        except TypeError:
+                            pass
+
+                    time.sleep(0.4)
+                except PIL.UnidentifiedImageError:
+                    pass
+
+print(show_cur_time('Finish'))
